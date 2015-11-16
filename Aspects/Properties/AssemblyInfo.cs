bd257621342f2ee0ae491d9fb7e64f9938a7c925
Val Melamed <valo@vtmelamed.com>
2015-11-16 18:47:47 -0500

    Changed version to 1.0.1-beta. Updated the version of Unity to 4.0.1.

--- Aspects/Properties/AssemblyInfo.cs
+++ Aspects/Properties/AssemblyInfo.cs
@@ -1,16 +1,16 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyTitle("vm.Aspects")]
-[assembly: AssemblyDescription("A set of classes addresing various common cross-cutting concerns.")]
-[assembly: AssemblyVersion("0.9.1")]
-[assembly: AssemblyFileVersion("0.9.1")]
-[assembly: AssemblyInformationalVersion("0.9.1")]
+[assembly: AssemblyDescription("A set of classes addressing various common cross-cutting concerns.")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.1")]
+[assembly: AssemblyInformationalVersion("1.0.1")]
 
 
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo(
     "vm.Aspects.Test, " +
-    "PublicKey=00240000048000009400000006020000002400005253413100040000010001004b151837822bdb"+
-              "b18296865918eb87882f2528630f673f17668fff4c6e51b8563b1985ec7ec5e6877611068c595a"+
-              "e297a32e3bf9d71a5b479a59705f0688e91f086f20fa0772135ee08f693d5310ce18761d4f71ed"+
-              "cb2486ec0910dd7361decc2184708dcbd5211feb361f45b024a45b91c28d7bed158671053085b8"+
+    "PublicKey=00240000048000009400000006020000002400005253413100040000010001004b151837822bdb" +
+              "b18296865918eb87882f2528630f673f17668fff4c6e51b8563b1985ec7ec5e6877611068c595a" +
+              "e297a32e3bf9d71a5b479a59705f0688e91f086f20fa0772135ee08f693d5310ce18761d4f71ed" +
+              "cb2486ec0910dd7361decc2184708dcbd5211feb361f45b024a45b91c28d7bed158671053085b8" +
               "fae385c1")]
