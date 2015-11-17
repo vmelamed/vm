238cff06496d5cffc52343ff8c3842a9d8061890
Val Melamed <valo@vtmelamed.com>
2015-11-16 19:24:19 -0500

    v1.0.1-beta

--- Aspects/NuGet/PublishAspects.cmd
+++ Aspects/NuGet/PublishAspects.cmd
@@ -1,10 +1,17 @@
 pushd
-cd %~dp0..
 NuGet Update -self
 call "%VS140COMNTOOLS%vsvars32.bat"
 if not .%1.==.. NuGet SetApiKey %1
+cd %~dp0..
 msbuild vm.Aspects.csproj /t:Rebuild /p:Configuration=Release /m
+cd Model
+msbuild vm.Aspects.Model.csproj /t:Rebuild /p:Configuration=Release /m
+cd ..\Parsers
+msbuild vm.Aspects.Parsers.csproj /t:Rebuild /p:Configuration=Release /m
+cd ..\Wcf
+msbuild vm.Aspects.Wcf.csproj /t:Rebuild /p:Configuration=Release /m
 if errorlevel 1 goto exit
+cd ..
 NuGet Pack NuGet\vm.Aspects.nuspec -Prop Configuration=Release
 if errorlevel 1 goto exit
 NuGet Push *.nupkg
