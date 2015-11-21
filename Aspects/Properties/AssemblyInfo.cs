88b0cb0a5ae5d30c33f259ae5ae3a09f7f16dc0d
Val Melamed <valo@vtmelamed.com>
2015-11-20 20:07:26 -0500

    Released 1.0.2-beta

--- Aspects/Properties/AssemblyInfo.cs
+++ Aspects/Properties/AssemblyInfo.cs
@@ -2,9 +2,9 @@
 
 [assembly: AssemblyTitle("vm.Aspects")]
 [assembly: AssemblyDescription("A set of classes addressing various common cross-cutting concerns.")]
-[assembly: AssemblyVersion("1.0.1")]
-[assembly: AssemblyFileVersion("1.0.1")]
-[assembly: AssemblyInformationalVersion("1.0.1")]
+[assembly: AssemblyVersion("1.0.2")]
+[assembly: AssemblyFileVersion("1.0.2")]
+[assembly: AssemblyInformationalVersion("1.0.2")]
 
 
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo(
