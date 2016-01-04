a1ab74f9472a8494d981ab96e76780b8be8b9bd7
vmelamed <valo@vtmelamed.com>
2016-01-04 04:11:47 -0500

    Added EntityRecordInfoDumpMetadata

---
+++ Aspects/Model/Metadata/EntityRecordInfoDumpMetadata.cs
@@ -0,0 +1,28 @@
+﻿using vm.Aspects.Diagnostics;
+
+namespace vm.Aspects.Model.Metadata
+{
+    /// <summary>
+    /// Class EntityRecordInfoDumpMetadata.
+    /// </summary>
+    public abstract class EntityRecordInfoDumpMetadata
+    {
+        /// <summary>
+        /// The entity key
+        /// </summary>
+        [Dump(false)]
+        public object EntityKey { get; set; }
+
+        /// <summary>
+        /// The field metadata
+        /// </summary>
+        [Dump(false)]
+        public object FieldMetadata { get; set; }
+
+        /// <summary>
+        /// The record type
+        /// </summary>
+        [Dump(false)]
+        public object RecordType { get; set; }
+    }
+}
