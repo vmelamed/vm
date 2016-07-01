69bb895422da071f7d99dae6443334bac1fa2e18
Val Melamed <valo@vtmelamed.com>
2016-06-30 22:48:21 -0400

    Bumped the versions

--- Aspects/Diagnostics/Properties/AssemblyInfo.cs
+++ Aspects/Diagnostics/Properties/AssemblyInfo.cs
@@ -2,9 +2,9 @@
 
 [assembly: AssemblyTitle("vm.Aspects.Diagnostics.ObjectDumper")]
 [assembly: AssemblyDescription("Dumps the properties' and fields' values of any .NET object in a text form.")]
-[assembly: AssemblyVersion("1.6.7")]
-[assembly: AssemblyFileVersion("1.6.7")]
-[assembly: AssemblyInformationalVersion("1.6.7")]
+[assembly: AssemblyVersion("1.6.8")]
+[assembly: AssemblyFileVersion("1.6.8")]
+[assembly: AssemblyInformationalVersion("1.6.8")]
 
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo(
     "vm.Aspects.Diagnostics.ObjectDumper.Tests, " +
