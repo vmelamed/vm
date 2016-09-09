af6b1f97117f2bc63a49e37a0b4293ba53605955
Val Melamed <valo@vtmelamed.com>
2016-09-09 16:02:07 -0400

    sync

--- Aspects/Properties/AssemblyInfo.cs
+++ Aspects/Properties/AssemblyInfo.cs
@@ -2,9 +2,9 @@
 
 [assembly: AssemblyTitle("vm.Aspects")]
 [assembly: AssemblyDescription("A set of classes addressing various common cross-cutting concerns.")]
-[assembly: AssemblyVersion("1.0.70")]
-[assembly: AssemblyFileVersion("1.0.70")]
-[assembly: AssemblyInformationalVersion("1.0.70")]
+[assembly: AssemblyVersion("1.0.71")]
+[assembly: AssemblyFileVersion("1.0.71")]
+[assembly: AssemblyInformationalVersion("1.0.71")]
 
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo(
     "vm.Aspects.Test, " +
