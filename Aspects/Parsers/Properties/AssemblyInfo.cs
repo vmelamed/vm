dc7186e2f8ed4a5b53a18982e35040fdc67061f1
Val Melamed <valo@vtmelamed.com>
2017-03-02 23:52:08 -0500

    bumped the version to 1.0.95

--- Aspects/Parsers/Properties/AssemblyInfo.cs
+++ Aspects/Parsers/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@
 [assembly: AssemblyTitle("vm.Aspects.Parser")]
 [assembly: AssemblyDescription("Text parsing readers, e.g. CSV/TSV reader.")]
 
-[assembly: AssemblyVersion("1.0.94")]
-[assembly: AssemblyFileVersion("1.0.94")]
-[assembly: AssemblyInformationalVersion("1.0.94")]
+[assembly: AssemblyVersion("1.0.95")]
+[assembly: AssemblyFileVersion("1.0.95")]
+[assembly: AssemblyInformationalVersion("1.0.95")]
 
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo(
     "vm.Aspects.Parsers.Tests, " +
