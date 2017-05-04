c74d7b6d6b702165863d04c62c8291e35cf521b1
Val Melamed <valo@vtmelamed.com>
2017-05-04 11:55:28 -0400

    Upgraded to the new dumper. v1.7 bumped the ver to v.1.0.104-beta

--- Aspects/NuGet/PublishAspects.cmd
+++ Aspects/NuGet/PublishAspects.cmd
@@ -1,7 +1,7 @@
 pushd
 if "%VSINSTALLDIR%"=="" call "%VS140COMNTOOLS%vsvars32.bat"
 set Configuration=Release
-set vmAspectsVersion=1.0.103-beta
+set vmAspectsVersion=1.0.104-beta
 
 cd %~dp0..
 del *.nupkg
