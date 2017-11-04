826f53a6833d52b2331ab16b813f7d2952cdc93b
Val Melamed <valo@vtmelamed.com>
2017-11-04 00:43:28 -0400

    cosmetic

--- Aspects/Threading/RetryConstants.cs
+++ Aspects/Threading/RetryConstants.cs
@@ -22,19 +22,22 @@ namespace vm.Aspects.Threading
         public const int DefaultMaxDelay = 150;
 
         /// <summary>
-        /// The default method testing if the operation has failed.
+        /// The default method testing if the operation has failed is: 
+        /// <code>
+        /// <![CDATA[public static bool DefaultIsFailure(T result, Exception exception, int attempt) => exception != null;]]>
+        /// </code>
         /// </summary>
         /// <param name="result">The result of the operation.</param>
         /// <param name="exception">The exception that was thrown by the operation (if any).</param>
         /// <param name="attempt">The number of the current attempt.</param>
         /// <returns><see langword="true" /> if the operation failed and cannot be retried, <see langword="false" /> otherwise.</returns>
-        /// <remarks>The default implementation is:
-        /// <code><![CDATA[public static bool DefaultIsFailure(T result, Exception exception, int attempt) => exception != null;]]></code>
-        /// </remarks>
         public static bool DefaultIsFailure<T>(T result, Exception exception, int attempt) => exception != null;
 
         /// <summary>
-        /// The default method testing if the operation has succeeded.
+        /// The default method testing if the operation has succeeded is: 
+        /// <code>
+        /// <![CDATA[public static bool DefaultIsSuccess(T result, Exception exception, int attempt) => exception == null;]]>
+        /// </code>
         /// </summary>
         /// <param name="result">The result of the operation.</param>
         /// <param name="exception">The exception that was thrown by the operation (if any).</param>
@@ -42,26 +45,24 @@ namespace vm.Aspects.Threading
         /// <returns><see langword="true" /> if the operation succeeded and should not be retried, <see langword="false" /> otherwise.</returns>
         /// <remarks>
         /// The default implementation is:
-        /// <code>
-        /// <![CDATA[public static bool DefaultIsSuccess(T result, Exception exception, int attempt) => exception == null;]]>
-        /// </code>
         /// </remarks>
         public static bool DefaultIsSuccess<T>(T result, Exception exception, int attempt) => exception == null;
 
         /// <summary>
-        /// The default epilogue method throws the raised exception or returns the result of the operation.
+        /// The default epilogue method throws the raised exception or returns the result of the operation:
+        /// <code>
+        /// <![CDATA[public static T DefaultEpilogue(T result, Exception exception, int attempt)
+        /// {
+        ///     if (exception!=null)
+        ///         throw exception;
+        ///     return result;
+        /// }]]>
+        /// </code>
         /// </summary>
         /// <param name="result">The result of the operation.</param>
         /// <param name="exception">The exception that was thrown by the operation (if any).</param>
         /// <param name="attempt">The number of the current attempt.</param>
         /// <returns>If the last operation threw an exception - this will re-throw it otherwise the result from the last operation.</returns>
-        /// <remarks>The default implementation is:
-        /// <code><![CDATA[public static T DefaultEpilogue(T result, Exception exception, int attempt)
-        /// {
-        ///     if (exception!=null)
-        ///         throw exception;
-        ///     return result;
-        /// }]]></code></remarks>
         public static T DefaultEpilogue<T>(T result, Exception exception, int attempt)
         {
             if (exception!=null)
@@ -70,44 +71,31 @@ namespace vm.Aspects.Threading
         }
 
         /// <summary>
-        /// The default method testing if the operation has failed.
+        /// The default method testing if the operation has failed is:
+        /// <code>
+        /// <![CDATA[public static Task<bool> DefaultIsFailure(T result, Exception exception, int attempt) => Task.FromResult(exception != null);]]>
+        /// </code>
         /// </summary>
         /// <param name="result">The result of the operation.</param>
         /// <param name="exception">The exception that was thrown by the operation (if any).</param>
         /// <param name="attempt">The number of the current attempt.</param>
         /// <returns><see langword="true" /> if the operation failed and cannot be retried, <see langword="false" /> otherwise.</returns>
-        /// <remarks>
-        /// The default implementation is:
-        /// <code>
-        /// <![CDATA[public static Task<bool> DefaultIsFailure(T result, Exception exception, int attempt) => Task.FromResult(exception != null);]]>
-        /// </code>
-        /// </remarks>
         public static Task<bool> DefaultIsFailureAsync<T>(T result, Exception exception, int attempt) => Task.FromResult(exception != null);
 
         /// <summary>
-        /// The default method testing if the operation has succeeded.
+        /// The default method testing if the operation has succeeded is:
+        /// <code>
+        /// <![CDATA[public static Task<bool> DefaultIsSuccessAsync<T>(T result, Exception exception, int attempt) => Task.FromResult(exception == null);]]>
+        /// </code>
         /// </summary>
         /// <param name="result">The result of the operation.</param>
         /// <param name="exception">The exception that was thrown by the operation (if any).</param>
         /// <param name="attempt">The number of the current attempt.</param>
         /// <returns><see langword="true" /> if the operation succeeded and should not be retried, <see langword="false" /> otherwise.</returns>
-        /// <remarks>
-        /// The default implementation is:
-        /// <code>
-        /// <![CDATA[public static Task<bool> DefaultIsSuccessAsync<T>(T result, Exception exception, int attempt) => Task.FromResult(exception == null);]]>
-        /// </code>
-        /// </remarks>
         public static Task<bool> DefaultIsSuccessAsync<T>(T result, Exception exception, int attempt) => Task.FromResult(exception == null);
 
         /// <summary>
-        /// The default epilogue method throws the raised exception or returns the result of the operation.
-        /// </summary>
-        /// <param name="result">The result of the operation.</param>
-        /// <param name="exception">The exception that was thrown by the operation (if any).</param>
-        /// <param name="attempt">The number of the current attempt.</param>
-        /// <returns>If the last operation threw an exception - this will re-throw it otherwise the result from the last operation.</returns>
-        /// <remarks>
-        /// The default implementation is:
+        /// The default epilogue method throws the raised exception or returns the result of the operation:
         /// <code>
         /// <![CDATA[public static Task<T> DefaultEpilogue(T result, Exception exception, int attempt)
         /// {
@@ -116,7 +104,11 @@ namespace vm.Aspects.Threading
         ///     return Task.FromResult(result);
         /// }]]>
         /// </code>
-        /// </remarks>
+        /// </summary>
+        /// <param name="result">The result of the operation.</param>
+        /// <param name="exception">The exception that was thrown by the operation (if any).</param>
+        /// <param name="attempt">The number of the current attempt.</param>
+        /// <returns>If the last operation threw an exception - this will re-throw it otherwise the result from the last operation.</returns>
         public static Task<T> DefaultEpilogueAsync<T>(T result, Exception exception, int attempt)
         {
             if (exception!=null)
