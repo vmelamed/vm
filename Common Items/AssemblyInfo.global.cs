5e4d1f1455e293a9fb3b0d123ddb1a859e7b5a3d
Val Melamed <valo@vtmelamed.com>
2018-07-27 20:42:29 -0400

    Playing with the project settings...

--- Common Items/AssemblyInfo.global.cs
+++ Common Items/AssemblyInfo.global.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Resources;
 using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
-[assembly: NeutralResourcesLanguage("")]
