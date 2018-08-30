8e57e57ba63d644ecda9af5ab58da5460d8a210e
Val Melamed <valo@vtmelamed.com>
2018-08-29 22:45:04 -0400

    Temp

---
+++ Aspects/Security/Cryptography/Ciphers/build/PublishCiphersProtected.cmd
@@ -0,0 +1,33 @@
+cd %~dp0..\ProtectedData\src
+
+if .%1. EQU .. (
+    set PackagePath=bin\Release\
+    set Configuration=--configuration Release
+    set VersionSuffix=
+) else (
+    set PackagePath=bin\Debug\
+    set Configuration=--configuration Debug
+    set VersionSuffix=--version-suffix %1
+    )
+
+rem ------- Build and Package -------
+dotnet clean %Configuration%
+dotnet pack --include-symbols --include-source %Configuration% %VersionSuffix%
+if errorlevel 1 goto exit
+
+rem ------- Publish locally -------
+if not exist c:\NuGet md c:\NuGet
+copy /y %PackagePath%*.nupkg c:\NuGet
+
+rem ------- Upload to NuGet.org -------
+
+@echo Press any key to push to NuGet.org or Ctrl-Break to exit... > con:
+@pause > nul:
+
+dotnet nuget push %PackagePath% --source https://www.nuget.org
+if errorlevel 1 goto exit
+if .%VersionSuffix%. EQU ..  AND .%VersionPrefix%. NEQ .. git tag %VersionPrefix%
+
+:exit
+cd ..\build
+pause
