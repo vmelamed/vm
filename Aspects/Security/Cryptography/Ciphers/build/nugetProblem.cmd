1fa6ea405c33e57aa591d3bfafe46f3b102c81cf
Val Melamed <valo@vtmelamed.com>
2018-09-03 01:22:08 -0400

    again...

--- Aspects/Security/Cryptography/Ciphers/build/nugetProblem.cmd
+++ Aspects/Security/Cryptography/Ciphers/build/nugetProblem.cmd
@@ -1,9 +1,11 @@
 rem This little script demo-s the NuGet problem of pushing the symbols package to the wrong URL
 rem Remember to bump the number of the beta, e.g. beta.22 -> beta.23
 
-cd %~dp0\..\src
+set suffix=beta.24
+
+cd %~dp0..\src
 del /q bin\Debug\*.nupkg
 dotnet clean --configuration Debug
-dotnet build --configuration Debug --version-suffix beta.22
-dotnet pack --include-symbols --include-source --configuration Debug --version-suffix beta.23
+dotnet build --configuration Debug --version-suffix %suffix%
+dotnet pack --include-symbols --include-source --configuration Debug --version-suffix %suffix%
 dotnet nuget push bin\Debug\ --source https://www.nuget.org
