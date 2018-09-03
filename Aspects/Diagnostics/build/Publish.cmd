ef528cf91637e157da123d6524ef1857717789f7
Val Melamed <valo@vtmelamed.com>
2018-09-03 16:58:23 -0400

    vm.Aspects.Security.Cryptography.Ciphers 2.1.4 published

--- Aspects/Diagnostics/build/Publish.cmd
+++ Aspects/Diagnostics/build/Publish.cmd
@@ -1,33 +1,36 @@
 cd %~dp0..\src
 
 if .%1. EQU .. (
-    set PackagePath=bin\Release\
-    set Configuration=--configuration Release
-    set VersionSuffix=
+    set $PackagePath=bin\Release\
+    set $Configuration=--configuration Release
+    set $VersionSuffix=
+    set $DebugIncludes=--include-symbols
 ) else (
-    set PackagePath=bin\Debug\
-    set Configuration=--configuration Debug
-    set VersionSuffix=--version-suffix %1
+    set $PackagePath=bin\Debug\
+    set $Configuration=--configuration Debug
+    set $VersionSuffix=--version-suffix %1
+    set $DebugIncludes=--include-symbols --include-source
     )
 
 rem ------- Build and Package -------
-dotnet clean %Configuration%
-dotnet build %Configuration%
-dotnet pack --include-symbols --include-source %Configuration% %VersionSuffix%
+dotnet clean %$Configuration%
+del %$PackagePath%*.nupkg
+dotnet build %$Configuration%
+dotnet pack %$DebugIncludes% %$Configuration% %$VersionSuffix%
 if errorlevel 1 goto exit
 
 rem ------- Publish locally -------
 if not exist c:\NuGet md c:\NuGet
-copy /y %PackagePath%*.nupkg c:\NuGet
+copy /y %$PackagePath%*.nupkg c:\NuGet
 
 rem ------- Upload to NuGet.org -------
 
 @echo Press any key to push to NuGet.org or Ctrl-Break to exit... > con:
 @pause > nul:
 
-dotnet nuget push %PackagePath% --source https://www.nuget.org
+dotnet nuget push %$PackagePath% --source https://www.nuget.org --symbol-source https://nuget.smbsrc.net
 if errorlevel 1 goto exit
-if .%VersionSuffix%. EQU ..  AND .%VersionPrefix%. NEQ .. git tag %VersionPrefix%
+if .%$VersionSuffix%. EQU ..  AND .%VersionPrefix%. NEQ .. git tag %VersionPrefix%
 
 :exit
 cd ..\build
--- Aspects/Security/Cryptography/Ciphers/build/buildProject.cmd
+++ Aspects/Security/Cryptography/Ciphers/build/buildProject.cmd
@@ -2,24 +2,24 @@ if .%1. EQU .. (
     set $PackagePath=bin\Release\
     set $Configuration=--configuration Release
     set $VersionSuffix=
-    set $DebugIncludes=
 ) else (
     set $PackagePath=bin\Debug\
     set $Configuration=--configuration Debug
     set $VersionSuffix=--version-suffix %1
-    set $DebugIncludes=--include-symbols --include-source
     )
 
 rem ------- Clean -------
 dotnet clean %$Configuration%
 del /q %$PackagePath%*.nupkg
+rd /s /q %$PackagePath%TempPackages
 
 rem ------- Build -------
 dotnet build %$Configuration% %$VersionSuffix%
 if errorlevel 1 pause & goto exit
+rem del /s /q %$PackagePath%*.deps.json
 
 rem ------- Package -------
-dotnet pack %$DebugIncludes% %$Configuration% %$VersionSuffix%
+dotnet pack --include-symbols --include-source %$Configuration% %$VersionSuffix%
 if errorlevel 1 pause & goto exit
 
 rem ------- Publish locally -------
@@ -32,15 +32,24 @@ if .%2. EQU .. (
 @pause > nul:
 )
 
-dotnet nuget push %$PackagePath% --source https://www.nuget.org/ --symbol-source https://nuget.smbsrc.net/ 
-rem nuget push %$PackagePath% -Source https://www.nuget.org/  -SymbolSource https://nuget.smbsrc.net/ -verbosity detailed
-if errorlevel 1 pause & goto exit
+md %$PackagePath%TempPackages
+move %$PackagePath%*.symbols.nupkg %$PackagePath%TempPackages
+dotnet nuget push %$PackagePath%*.nupkg --source https://www.nuget.org/
+del %$PackagePath%*.nupkg
+move %$PackagePath%TempPackages\*.symbols.nupkg %$PackagePath%
+dotnet nuget push %$PackagePath%*.nupkg --source https://nuget.smbsrc.net
+del /q /s %$PackagePath%*.nupkg
+rd /s /q %$PackagePath%TempPackages
 
-if .%$VersionSuffix%. EQU .. (
-    git tag vm.Aspects.Security.Cryptography.Ciphers.ProtectedData-%VersionPrefix%
-) else (
-    git tag vm.Aspects.Security.Cryptography.Ciphers.ProtectedData-%VersionPrefix%-%$VersionSuffix%
-)
+:if .%$VersionSuffix%. EQU .. (
+:    git tag vm.Aspects.Security.Cryptography.Ciphers.ProtectedData-%VersionPrefix%
+:) else (
+:    git tag vm.Aspects.Security.Cryptography.Ciphers.ProtectedData-%VersionPrefix%-%$VersionSuffix%
+:)
 
 :exit
+set $PackagePath
+set $Configuration=
+set $VersionSuffix=
+
 if not errorlevel 1 if .%2. EQU .. pause
